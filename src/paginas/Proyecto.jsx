03b52931c081d59723eb81922bf4e2f0ac93885e
Alex <alexbastidas.dev@gmail.com>
2022-11-23 17:56:16 -0500

    ajustes responsive

--- src/paginas/Proyecto.jsx
+++ src/paginas/Proyecto.jsx
@@ -68,29 +68,31 @@ const Proyecto = () => {
   return (
     <>
 
-      <div className='flex justify-between'>
+      <div className='flex justify-between items-center'>
         <h1 className='font-black text-4xl text-[#4B1D89]'>{nombre}</h1>
 
         {admin && (
-          <div className='flex gap-7'>
-            <div className='flex items-center gap-1 text-gray-400 hover:text-black'>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-              </svg>
-              <Link
-                to={`/proyectos/editar/${params.id}`}
-                className='uppercase font-bold'
-              >Editar</Link>
-            </div>
+          <div className=''>
+            <div className='sm:flex sm:my-3'>
+              <div className='flex items-center gap-1 text-gray-400 hover:text-black'>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                </svg>
+                <Link
+                  to={`/proyectos/editar/${params.id}`}
+                  className='uppercase font-bold'
+                >Editar</Link>
+              </div>
 
-            <div className='flex items-center gap-2 text-gray-400 hover:text-black'>
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-              </svg>
-              <button
-                className='uppercase font-bold'
-                onClick={handleClick}
-              >Eliminar</button>
+              <div className='flex items-center gap-2 text-gray-400 hover:text-black'>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+                <button
+                  className='uppercase font-bold'
+                  onClick={handleClick}
+                >Eliminar</button>
+              </div>
             </div>
 
           </div>
