261c3eb1940dea0c035d04ddaf4cb4331aaa0cb4
Alex <alexbastidas.dev@gmail.com>
2022-11-23 17:26:06 -0500

    wefw

--- src/paginas/Proyecto.jsx
+++ src/paginas/Proyecto.jsx
@@ -73,7 +73,7 @@ const Proyecto = () => {
 
         {admin && (
           <div className='flex gap-7'>
-            <div className='flex items-center gap-2 text-gray-400 hover:text-black'>
+            <div className='flex items-center gap-1 text-gray-400 hover:text-black'>
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
               </svg>
@@ -103,7 +103,7 @@ const Proyecto = () => {
           onClick={handleModalTarea}
           type='button'
           className='text-sm px-5 py-3 w-full md:w-auto rounded-lg uppercase font-bold bg-[#1B053B] text-white text-center mt-5 flex gap-2 items-center justify-center hover:bg-[#F5C116]'
-          >
+        >
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
           </svg>
@@ -117,7 +117,7 @@ const Proyecto = () => {
       {msg && <Alerta alerta={alerta} />}
 
       <div className='bg-white shadow mt-10 rounded-lg'>
-        {proyecto.tareas?.length ? 
+        {proyecto.tareas?.length ?
           proyecto.tareas?.map(tarea => (
             <Tarea
               key={tarea._id}
@@ -133,8 +133,11 @@ const Proyecto = () => {
             <p className='font-bold text-xl'>Colaboradores</p>
             <Link
               to={`/proyectos/nuevo-colaborador/${proyecto._id}`}
-              className='text-gray-400 hover:text-black uppercase font-bold'
-            >Añadir</Link>
+              className='text-gray-400 hover:text-black uppercase font-bold mr-3 flex gap-1'>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd" />
+              </svg> Añadir
+            </Link>
           </div>
 
           <div className='bg-white shadow mt-10 rounded-lg'>
