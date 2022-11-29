ee303e8a4c448fea1e9df783f64093cc56dea240
Neybran <neybran@gmail.com>
2022-11-29 13:15:29 -0500

    otra linea modificada en home.jsx

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -59,6 +59,7 @@ const Home = () => {
                             <div className="w-full sm:w-1/2 p-6 mt-6">
                             <img src="tarea.png" alt="" />
                           </div>
+                          <br/><br/>
                             <div className="w-full sm:w-1/2 p-6 mt-6">
                                 <div className="align-middle">
                                     <h3 className="text-3xl text-gray-800 font-bold leading-none mb-3">
